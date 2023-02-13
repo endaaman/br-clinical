f39df6a0034e103674e703d6f5625c7813f582ee
endaaman <ken@endaaman.me>
2023-02-13 17:32:51 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import re
 
 import click
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+import lightgbm as lgb
+
+from endaaman import Timer
+from endaaman.torch import fix_random_states, get_global_seed
 
 primary_plain = '非造影超音波'
 
@@ -49,8 +55,8 @@ enhance_lymph_cols = [
     '造影超音波/リンパ節_lymphsize_短径',
     *[f'造影超音波/リンパ節_term_{i}' for i in range(1, 9)],
     *[f'造影超音波/リンパ節_B_{i}' for i in range(1, 6)],
-    '造影超音波/リンパ節_PI_7',
-    '造影超音波/リンパ節_PI_実数',
+    # '造影超音波/リンパ節_PI_7',
+    # '造影超音波/リンパ節_PI_実数',
 ]
 
 
@@ -71,17 +77,83 @@ def load_data():
     ]
     return df
 
+def train_model(x_train, y_train, x_valid, y_valid, fold):
+    gbm_params = {
+        'objective': 'binary',
+        'num_threads': -1,
+        'max_depth': 3,
+        'bagging_seed': get_global_seed(),
+        'random_state': get_global_seed(),
+        'boosting': 'gbdt',
+        'metric': 'auc',
+        'verbosity': -1,
+    }
+
+    train_data = lgb.Dataset(x_train, label=y_train, categorical_feature=[])
+    valid_sets = [train_data]
+    if np.any(x_valid):
+        valid_data = lgb.Dataset(x_valid, label=y_valid, categorical_feature=[])
+        valid_sets += [valid_data]
+
+    model = lgb.train(
+        gbm_params, # モデルのパラメータ
+        train_data, # 学習データ
+        num_boost_round=10000,  # 最大学習サイクル数。early_stopping使用時は大きな値を入力
+        valid_sets=valid_sets,
+        # early_stopping_rounds=150,
+        callbacks=[
+            lgb.early_stopping(stopping_rounds=10, verbose=False),
+            lgb.log_evaluation(False)
+        ],
+        categorical_feature=[],
+    )
+
+    tmp = pd.DataFrame()
+    tmp['feature'] = cols_feature
+    tmp['importance'] = model.feature_importance(importance_type='gain')
+    # tmp['importance'] = model.feature_importance(importance_type='split')
+    self.df_importances.append(tmp)
+
+    return model
+
+
+def train_data(df_train, df_test, num_folds=5):
+    t = Timer()
+    t.start()
+    folds = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=get_global_seed())
+    folds = folds.split(np.arange(len(df_train)), y=df_train[target_col])
+    folds = list(folds)
+    models = []
+    for fold in range(num_folds):
+        # print(f'fold {fold+1}/{num_folds}')
+        df_x = df_train.drop([target_col], axis=1)
+        df_y =  df_train[target_col]
+        vv = [
+            df_x.iloc[folds[fold][0]], # x_train
+            df_y.iloc[folds[fold][0]], # y_train
+            df_x.iloc[folds[fold][1]], # x_valid
+            df_y.iloc[folds[fold][1]], # y_valid
+        ]
+        vv = [v.copy() for v in vv]
+        model = train_model(*vv, fold)
+        models.append(model)
+    t.end()
+    print(f'Training time: {t.ms()}')
+    return models
+
 
 @click.group()
 def cli():
-    pass
+    fix_random_states(42)
 
 @cli.command()
 # @click.option('--mil', 'use_mil', is_flag=True)
 def train():
     df = load_data()
-    df_p = df[plain_cols]
-    df_pe = df[plain_cols + enhance_cols]
+    dfs = {
+        'p': df[plain_cols],
+        'pe': df[plain_cols + enhance_cols],
+    }
     print(df_p)
 
 
