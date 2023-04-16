56edff9ac01eedcf791328993a0b7203487f1d96
endaaman <ken@endaaman.me>
2023-04-17 00:25:10 +0900

    update

--- scripts/move.mjs
+++ scripts/move.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env zx
 
+await $`mkdir -p tmp/rocs_t1`
+
 let i = 0
 while (i < 100) {
-  await $`mv out20230410_${i}_t0/roc_181E.png tmp/rocs/roc${i}.png`
+  await $`mv out20230410_${i}_t1/roc_1E18.png tmp/rocs_t1/roc${i}.png`
   i += 1
 }
--- scripts/seed.mjs
+++ scripts/seed.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env zx
 
+const thres = 1
 let i = 0
+
 while (i < 100) {
-  await $`python main.py gbm --seed ${i}`
+  await $`python main.py gbm --seed ${i} --thres ${thres}`
   i += 1
 }
